664cb18854ca6ceb6f8e0848e1942e6f96df3f87
hasanAntakly <105155021+hasanAntakly@users.noreply.github.com>
2022-12-25 11:26:24 +0200

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './View/home/home.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { HomeComponent } from './View/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+		MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
