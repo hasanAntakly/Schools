201518b94017c09bc239d3114593b6fea24326a0
hasanAntakly <105155021+hasanAntakly@users.noreply.github.com>
2022-12-26 14:33:37 +0200

    installed font awesome successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { MatIconModule } from '@angular/material/icon';
 		MatSidenavModule,
 		MatButtonModule,
 		MatIconModule,
+  	FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
