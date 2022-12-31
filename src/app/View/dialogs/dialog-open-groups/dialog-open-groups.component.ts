c433418da5d3595b474dc82bc36acbe1c8cd57f8
hasanAntakly <105155021+hasanAntakly@users.noreply.github.com>
2022-12-31 12:27:33 +0200

    finished the open groups dialog

--- src/app/View/dialogs/dialog-open-groups/dialog-open-groups.component.ts
+++ src/app/View/dialogs/dialog-open-groups/dialog-open-groups.component.ts
@@ -1,14 +1,39 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faFrown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dialog-open-groups',
   templateUrl: './dialog-open-groups.component.html',
-  styleUrls: ['./dialog-open-groups.component.css']
+  styleUrls: ['./dialog-open-groups.component.css'],
+	animations: [
+		trigger('HideTrigger', [
+			// ...
+			state('Hide', style({
+			})),
+			state('DontHide', style({
+				height: '0px',
+				opacity: '0'
+			})),
+			transition('Hide => DontHide', [
+				animate('0.5s')
+			]),
+			transition('DontHide => Hide', [
+				animate('0.5s')
+			]),
+		]),
+	]
 })
 export class DialogOpenGroupsComponent implements OnInit {
 
 	faT = faTimes;
+	faAL = faArrowLeft;
+	faF = faFrown;
+
+	PickedSE = false;
+	
 	ClickedYear = '';
 	ClickedEduLvl = '';
 
@@ -40,6 +65,31 @@ export class DialogOpenGroupsComponent implements OnInit {
 		{EL: '11th Grade'},
 	];
 
+	th52021: NumStuLft[] = [
+		{Name: 'A', Number: '10'},
+		{Name: 'B', Number: '7'},
+		{Name: 'C', Number: '12'},
+		{Name: 'A', Number: '10'},
+		{Name: 'B', Number: '7'},
+		{Name: 'C', Number: '12'},
+		{Name: 'A', Number: '10'},
+		{Name: 'B', Number: '7'},
+		{Name: 'C', Number: '12'},
+		{Name: 'A', Number: '10'},
+		{Name: 'B', Number: '7'},
+		{Name: 'C', Number: '12'},
+		{Name: 'A', Number: '10'},
+		{Name: 'B', Number: '7'},
+		{Name: 'C', Number: '12'},
+	]
+
+	th72022: NumStuLft[] = [
+		{Name: 'A', Number: '2'},
+		{Name: 'B', Number: '6'},
+	]
+
+	ListShow: NumStuLft[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -49,15 +99,43 @@ export class DialogOpenGroupsComponent implements OnInit {
 		this.ClickedYear = year;
 	}
 
+	BackyearClicked(){
+		this.ClickedYear = '';
+	}
+
 	eduClicked(EL: string){
 		this.ClickedEduLvl = EL;
 	}
+
+	BackEduLvlClicked(){
+		this.ClickedEduLvl = '';
+	}
+
+	DoneClicked(){
+		if(this.ClickedYear == "2020/2021" && this.ClickedEduLvl == '5th Grade'){
+			this.ListShow = this.th52021;
+		}else if(this.ClickedYear == "2021/2022" && this.ClickedEduLvl == '7th Grade'){
+			this.ListShow = this.th72022;
+		}
+		else{
+			this.ListShow = [];
+		}
+		this.PickedSE = true;
+	}
+
+	ChangeClicked(){
+		this.PickedSE = false;
+	}
 }
+
+class NumStuLft {  
+	Name = '';
+	Number = '';
+}  
 
 class SYITEMS {  
 	Year = '';  
 }  
-
 
 class ELITEMS {  
 	EL = '';  
