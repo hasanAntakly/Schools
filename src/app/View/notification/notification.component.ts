5bb4bf3088caa55933326d3a4c13a3f3aceea097
hasanAntakly <105155021+hasanAntakly@users.noreply.github.com>
2023-03-19 14:22:10 +0200

    17/3

--- src/app/View/notification/notification.component.ts
+++ src/app/View/notification/notification.component.ts
@@ -1,15 +1,61 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { faChalkboardTeacher, faUserGraduate, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.css']
+  styleUrls: ['./notification.component.css'],
+	animations: [
+		trigger('HideTrigger', [
+			// ...
+			state('DontHide', style({
+				transform: 'translateY(0)',
+				opacity: '1'
+			})),
+			state('Hide', style({
+				transform: 'translateY(-100%)',
+				opacity: '0'
+			})),
+			transition('Hide => DontHide', [
+				animate('0.3s')
+			]),
+			transition('DontHide => Hide', [
+				animate('0.3s')
+			]),
+		]),
+		trigger('DoubleHideTrigger', [
+			// ...
+			state('DontHide', style({
+				transform: 'translateY(0)',
+				opacity: '1'
+			})),
+			state('Hide', style({
+				transform: 'translateY(100%)',
+				opacity: '0',
+			})),
+			transition('Hide => DontHide', [
+				animate('0.3s')
+			]),
+			transition('DontHide => Hide', [
+				animate('0.5s')
+			]),
+		]),
+	]
 })
 export class NotificationComponent implements OnInit {
 
   constructor() { }
 
+	Clickedbtn = '';
+	ClickedStudentbtn = '';
+
+	faCT = faChalkboardTeacher;
+	faC = faCheck;
+	faUG = faUserGraduate;
+
   ngOnInit(): void {
   }
+
 
 }
